9dfd5ff436d445eed5073c47b5a2671652f9ead9
rishabhp1993 <rishabhprakash2@gmail.com>
2020-07-20 21:47:26 +0530

    Editable title and content

--- scripts/post.js
+++ scripts/post.js
@@ -1,19 +1,29 @@
 var innertext;
+var innertextcontent;
 
 function makeeditable() {
     innertext = document.getElementById("titletext").innerText;
+    innertextcontent = document.getElementById("txtcontent").innerText;
     document.getElementById("titletextbox").children[0].value = innertext;
+    document.getElementById("txtareacontent").value = innertextcontent;
     document.getElementById("titletext").style.display = "none";
     document.getElementById("titletextbox").style.display = "block";
+    document.getElementById("txtcontent").style.display = "none";
+    document.getElementById("txtareacontent").style.display = "block";
     document.getElementById("btnedit").style.display = "none";
     document.getElementById("btnsave").style.display = "block";
 
 }
 
 function save() {
-    innertext = document.getElementById("titletext").innerText = innertext;
+    innertext = document.getElementById("titletextbox").children[0].value;
+    innertextcontent = document.getElementById("txtareacontent").value;
+    document.getElementById("titletext").innerText = innertext;
+    document.getElementById("txtcontent").innerText = innertextcontent;
     document.getElementById("titletext").style.display = "block";
+    document.getElementById("txtcontent").style.display = "block";
     document.getElementById("titletextbox").style.display = "none";
+    document.getElementById("txtareacontent").style.display = "none";
     document.getElementById("btnedit").style.display = "block";
     document.getElementById("btnsave").style.display = "none";
 }
